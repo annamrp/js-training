8740e34bc0b4ff4b8b8a588d783c334f1861ce52
Anna Ruiz <anna.ruiz@xing.com>
2019-05-22 15:30:12 +0200

    Add multiplication and tests

--- src/Calculator/Tokenizer.js
+++ src/Calculator/Tokenizer.js
@@ -11,7 +11,25 @@ const tokenize = val => {
 
   for (let i = 0; i < array.length; i++) {
     if (array[i] !== "+" && array[i] !== "-" && array[i] !== "*") {
-      accumulator += array[i];
+      if (array[i + 1] === "*") {
+        const currentValue = array[i];
+        const nextValue = array[i + 2];
+        const multSymbol = symbol[array[i + 1]];
+        const tokenizedMult = {
+          type: multSymbol,
+          value1: tokenize(currentValue),
+          value2: tokenize(nextValue)
+        };
+
+        if (array.length - 1 === i + 2) return tokenizedMult;
+        return {
+          type: symbol[array[i + 3]],
+          value1: tokenizedMult,
+          value2: tokenize(array.slice(i + 4).join(""))
+        };
+      }
+
+      accumulator = array[i];
 
       if (i === array.length - 1) {
         return { value: Number(accumulator), type: "value" };
--- test/Calculator/Tokenizer.test.js
+++ test/Calculator/Tokenizer.test.js
@@ -14,6 +14,14 @@ test("tokenizer recives a sum of numbers and returns a token", () => {
   });
 });
 
+test("tokenizer recives a rest of numbers and returns a token", () => {
+  expect(tokenize("5-3")).toEqual({
+    type: "rest",
+    value1: { value: 5, type: "value" },
+    value2: { value: 3, type: "value" }
+  });
+});
+
 test("tokenizer recives a mult of numbers and returns a token", () => {
   expect(tokenize("5*3")).toEqual({
     type: "mult",
@@ -22,17 +30,41 @@ test("tokenizer recives a mult of numbers and returns a token", () => {
   });
 });
 
-// test("tokenizer recives a mult and sum of numbers and returns a token", () => {
-//   expect(tokenize("5*5+3")).toEqual({
-//     type: "sum",
-//     value1: {
-//       type: "mult",
-//       value1: { value: 5, type: "value" },
-//       value2: { value: 5, type: "value" }
-//     },
-//     value2: { value: 3, type: "value" }
-//   });
-// });
+test("tokenizer recives a mult and sum of numbers and returns a token", () => {
+  expect(tokenize("5*5+3")).toEqual({
+    type: "sum",
+    value1: {
+      type: "mult",
+      value1: { value: 5, type: "value" },
+      value2: { value: 5, type: "value" }
+    },
+    value2: { value: 3, type: "value" }
+  });
+});
+
+test("tokenizer recives a sum and mult of numbers and returns a token", () => {
+  expect(tokenize("5+5*3")).toEqual({
+    type: "sum",
+    value1: { value: 5, type: "value" },
+    value2: {
+      type: "mult",
+      value1: { value: 5, type: "value" },
+      value2: { value: 3, type: "value" }
+    }
+  });
+});
+
+test("tokenizer recives a mult of 3 numbers and returns a token", () => {
+  expect(tokenize("5*5*3")).toEqual({
+    type: "mult",
+    value1: {
+      type: "mult",
+      value1: { value: 5, type: "value" },
+      value2: { value: 5, type: "value" }
+    },
+    value2: { value: 3, type: "value" }
+  });
+});
 
 test("eval recives a value token and returns its value", () => {
   expect(evaluator({ value: 8, type: "value" })).toEqual(8);
@@ -76,6 +108,20 @@ test("eval recives a rest token and returns its value", () => {
   expect(evaluator(sumToken)).toEqual(8);
 });
 
+test("eval recives a mult token and returns its value", () => {
+  const sumToken = {
+    type: "mult",
+    value1: {
+      type: "mult",
+      value1: { value: 7, type: "value" },
+      value2: { value: 3, type: "value" }
+    },
+    value2: { value: 2, type: "value" }
+  };
+
+  expect(evaluator(sumToken)).toEqual(42);
+});
+
 test("runs the program", () => {
   const result = evaluator(tokenize("55+25-75"));
 
@@ -86,4 +132,22 @@ test("runs the program", () => {
   const result = evaluator(tokenize("50+25+75"));
 
   expect(result).toBe(150);
+});
+
+test("runs the program", () => {
+  const result = evaluator(tokenize("5*5*5"));
+
+  expect(result).toBe(125);
+});
+
+test("runs the program", () => {
+  const result = evaluator(tokenize("5+5*5"));
+
+  expect(result).toBe(30);
+});
+
+test("runs the program", () => {
+  const result = evaluator(tokenize("5*5+3-1"));
+
+  expect(result).toBe(27);
 });
