ef08bd771e87a04f80a37ccc34e7880af3305a92
Anna Ruiz <anna.ruiz@xing.com>
2019-05-23 12:44:17 +0200

    Rename variables in tokenizer

--- src/Calculator/Tokenizer.js
+++ src/Calculator/Tokenizer.js
@@ -11,22 +11,27 @@ const tokenize = val => {
   };
 
   for (let i = 0; i < array.length; i++) {
-    if (array[i] !== "+" && array[i] !== "-" && array[i] !== "*") {
+    if (
+      array[i] !== "+" &&
+      array[i] !== "-" &&
+      array[i] !== "*" &&
+      array[i] !== "/"
+    ) {
       if (array[i + 1] === "*" || array[i + 1] === "/") {
         const currentValue = array[i];
         const nextValue = array[i + 2];
-        const multSymbol = symbol[array[i + 1]];
-        const tokenizedMult = {
-          type: multSymbol,
+        const operator = symbol[array[i + 1]];
+        const tokenizedOperation = {
+          type: operator,
           value1: tokenize(currentValue),
           value2: tokenize(nextValue)
         };
 
-        if (array.length - 1 === i + 2) return tokenizedMult;
+        if (array.length - 1 === i + 2) return tokenizedOperation;
 
         return {
           type: symbol[array[i + 3]],
-          value1: tokenizedMult,
+          value1: tokenizedOperation,
           value2: tokenize(array.slice(i + 4).join(""))
         };
       }
